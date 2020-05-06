d27ebd71b0a52fd2c1a8b4604344f0f9530e89e1
asharijuang <asharijuang@me.com>
2020-05-06 17:10:25 +0700

    receive webhook tickete and send message

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ import os
 app = Chalice(app_name='WallsDefaultAgentAllocation')
 app.debug = True
 
-base_url = "https://multichannel.qiscus.com"
-admin_token = "N4tYDSlsJNc9VvNLcr2d"
+QISCUS_BASE_URL = os.getenv("QISCUS_BASE_URL")
+QISMO_BASE_URL = os.getenv("QISMO_BASE_URL")
+QISMO_APP_ID = os.getenv("QISMO_APP_ID")
+QISMO_AUTH_TOKEN = os.getenv("QISMO_AUTH_TOKEN")
+QISMO_ADMIN_EMAIL = os.getenv("QISMO_ADMIN_EMAIL")
+
 
 @app.route('/', methods=['POST'])
 def index():
@@ -61,13 +65,13 @@ def index():
     return allocation.json()
 
 def allocate(room_id, agent_id):
-    url = f"{base_url}/api/v1/admin/service/assign_agent"
+    url = f"{QISMO_BASE_URL}/api/v1/admin/service/assign_agent"
     payload = {
             'room_id':room_id,
             'agent_id':int(agent_id)
         }
     headers = {
-        'Authorization': admin_token,
+        'Authorization': QISMO_AUTH_TOKEN,
         }
     
     response = requests.request("POST", url, data=payload, headers=headers)
@@ -78,9 +82,45 @@ def additionalInformation(room_id, userInfo):
         "user_properties": userInfo
     }
     print(f"send additionalInformation {payload}")
-    url = f"{base_url}/api/v1/qiscus/room/{room_id}/user_info"
+    url = f"{QISMO_BASE_URL}/api/v1/qiscus/room/{room_id}/user_info"
     headers = {
-        'Authorization': admin_token,
+        'Authorization': QISMO_AUTH_TOKEN,
     }
     response = requests.post(url, headers=headers, json=payload)
     return response
+
+@app.route('/ticket', methods=['POST'])
+def submitTicket():
+    body = app.current_request.json_body
+    print(f"body: {body}")
+
+    add_info = body['additional_info']
+    room_id = body['room_id']
+    message = f"""
+    Order ID: {room_id}
+    """
+    for i in add_info:
+        new = f"{i['key']} : {i['value']} \n"
+        message = message + new
+    
+    sendMessage(room_id, message)
+
+def sendMessage(room_id, message):
+    print(f"send {message} \nto {room_id}")
+    
+    url = f"{QISMO_BASE_URL}/{QISMO_APP_ID}/bot"
+    payload = {
+        "sender_email": QISMO_ADMIN_EMAIL, 
+        "message": message,
+        "type": "text",
+        "room_id": room_id
+    }
+    headers = {
+        'Authorization': QISMO_AUTH_TOKEN,
+        }
+    
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response
+
+
+
